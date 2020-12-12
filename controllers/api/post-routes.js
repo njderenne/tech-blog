e0ca5766e18e63bd0d8791b7aa151b1ac38a0d32
In Code We Trust <njderenne@gmail.com>
2020-12-12 00:31:06 -0600

    created a homepage with routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { Post, User, Vote, Comment } = require('../../models');
+const { Post, User, Comment } = require('../../models');
 
 
 router.get('/', (req, res) => {
